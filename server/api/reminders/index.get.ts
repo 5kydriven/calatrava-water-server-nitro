82d6fe630307309525b70a64b819fbcf6ac4872e
5kydriven <tagpuno619@gmail.com>
2025-03-20 17:45:44 +0800

    fix: seperate specific reminder and all reminders

--- server/api/reminders/index.get.ts
+++ server/api/reminders/index.get.ts
@@ -3,14 +3,11 @@ import { H3Event } from 'h3';
 
 export default defineEventHandler(async (event: H3Event) => {
 	const db = getFirestore();
-	const params = getRouterParam(event, 'uid');
 
 	try {
 		let remindersSnap = await db
 			.collection('reminders')
 			.orderBy('createdAt', 'desc')
-			.where('residentUid', '==', params)
-			.limit(1)
 			.get();
 
 		// Process the query results
