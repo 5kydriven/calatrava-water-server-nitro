e582a3f4cdf5a9061b854355ddcd1533ead822be
5kydriven <tagpuno619@gmail.com>
2025-04-07 21:40:06 +0800

    feat: add count of unread notifications to notifications API response

--- server/api/notifications/index.get.ts
+++ server/api/notifications/index.get.ts
@@ -9,6 +9,12 @@ export default defineEventHandler(async (event: H3Event) => {
 			.orderBy('createdAt', 'desc')
 			.get();
 
+		const count = await db
+			.collection('notifications')
+			.where('isRead', '==', 'false')
+			.count()
+			.get();
+
 		const notifications = snapshot.docs.map((doc) => ({
 			...doc.data(),
 			uid: doc.id,
@@ -16,6 +22,7 @@ export default defineEventHandler(async (event: H3Event) => {
 
 		return successResponse({
 			data: notifications,
+			total: count.data().count,
 		});
 	} catch (error: any) {
 		return errorResponse(error);
