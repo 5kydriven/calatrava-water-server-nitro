61d3013815d543241a448d938b2fa54862aae330
ccwebgroup <ccwebgroup@gmail.com>
2025-10-09 23:12:35 +0800

    feature: list users api

--- server/api/v1/users/index.get.ts
+++ server/api/v1/users/index.get.ts
@@ -1,8 +1,12 @@
 import { getAuth } from "firebase-admin/auth";
 
-
 export default defineEventHandler(async (event) => {
     const auth = getAuth();
+    
+    if (!auth) {
+        throw new Error("Firebase Auth not initialized");
+    }
+
     const users = await auth.listUsers();
     return users.users.map(user => ({
         uid: user.uid,
